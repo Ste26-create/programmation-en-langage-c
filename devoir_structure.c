31fb7a1482e1f4d62e0ba8665a4a5ddef737d2af
Ste26-create <tojoeanom@gmail.com>
2026-05-29 08:53:51 +0300

    Update devoir_structure.c

--- devoir_structure.c
+++ devoir_structure.c
@@ -36,12 +36,24 @@ void inscription(struct infopersonnel etudiant[]) {
         for (int i = 0; i < 6; i++) {
             printf("\n%s\t", lesinfo[i]);
             switch (i) {
-                case 0: scanf("%s", etudiant[j].nom); break;
-                case 1: scanf(" %[^\n]", etudiant[j].prenom); break;
-                case 2: scanf("%s", etudiant[j].date_de_naissance); break;
-                case 3: scanf(" %[^\n]", etudiant[j].adresse); break;
-                case 4: scanf(" %[^\n]", etudiant[j].niveau); break;
-                case 5: scanf(" %[^\n]", etudiant[j].filiere); break;
+                case 0: 
+                      scanf("%s", etudiant[j].nom); 
+                      break;
+                case 1: 
+                      scanf(" %[^\n]", etudiant[j].prenom); 
+                      break;
+                case 2: 
+                      scanf("%s", etudiant[j].date_de_naissance); 
+                      break;
+                case 3: 
+                      scanf(" %[^\n]", etudiant[j].adresse); 
+                      break;
+                case 4: 
+                      scanf(" %[^\n]", etudiant[j].niveau); 
+                      break;
+                case 5: 
+                      scanf(" %[^\n]", etudiant[j].filiere); 
+                      break;
             }
         }
 
@@ -50,29 +62,69 @@ void inscription(struct infopersonnel etudiant[]) {
         char num = etudiant[j].niveau[1];
 
         if (c == 'L' || c == 'l') {
-            if (num == '1') { etudiantL1[nb_eleveL1] = etudiant[j]; classe = 0; nb_eleveL1++; }
-            else if (num == '2') { etudiantL2[nb_eleveL2] = etudiant[j]; classe = 1; nb_eleveL2++; }
-            else if (num == '3') { etudiantL3[nb_eleveL3] = etudiant[j]; classe = 2; nb_eleveL3++; }
-        } else if (c == 'M' || c == 'm') {
-            if (num == '1') { etudiantM1[nb_eleveM1] = etudiant[j]; classe = 3; nb_eleveM1++; }
-            else if (num == '2') { etudiantM2[nb_eleveM2] = etudiant[j]; classe = 4; nb_eleveM2++; }
-        } else if (c == 'D' || c == 'd') {
-            if (num == '1') { etudiantD1[nb_eleveD1] = etudiant[j]; classe = 5; nb_eleveD1++; }
-            else if (num == '2') { etudiantD2[nb_eleveD2] = etudiant[j]; classe = 6; nb_eleveD2++; }
+            if (num == '1') 
+            { 
+              etudiantL1[nb_eleveL1] = etudiant[j]; 
+              classe = 0;
+              nb_eleveL1++; 
+            }
+            else if (num == '2') 
+            { 
+              etudiantL2[nb_eleveL2] = etudiant[j];
+              classe = 1; nb_eleveL2++; 
+            }
+            else if (num == '3') 
+            { 
+              etudiantL3[nb_eleveL3] = etudiant[j]; 
+              classe = 2; nb_eleveL3++; 
+            }
+        } 
+        else if (c == 'M' || c == 'm') 
+        {
+            if (num == '1') 
+            { 
+              etudiantM1[nb_eleveM1] = etudiant[j]; 
+              classe = 3;
+              nb_eleveM1++; 
+            }
+            else if (num == '2') 
+            { 
+              etudiantM2[nb_eleveM2] = etudiant[j]; 
+              classe = 4; 
+              nb_eleveM2++; 
+            }
+        } 
+        else if (c == 'D' || c == 'd') 
+        {
+            if (num == '1') 
+            { 
+              etudiantD1[nb_eleveD1] = etudiant[j]; 
+              classe = 5; 
+              nb_eleveD1++; 
+            }
+            else if (num == '2') 
+            { 
+              etudiantD2[nb_eleveD2] = etudiant[j]; 
+              classe = 6; 
+              nb_eleveD2++;
+            }
         }
 
         
-        if (classe != -1) {
+        if (classe != -1) 
+        {
             niveau_file = fopen(Niveau[classe], "a+");
             if (niveau_file == NULL) {
                 printf("impossible d'ouvrir le fichier\n");
                 exit(0);
-            }
-            fprintf(niveau_file, "%-20s %-25s %-20s %-20s %-20s\n", 
+        }
+        fprintf(niveau_file, "%-20s %-25s %-20s %-20s %-20s\n", 
                 etudiant[j].nom, etudiant[j].prenom, etudiant[j].date_de_naissance, etudiant[j].niveau, etudiant[j].filiere);
             fclose(niveau_file);
-            printf("\nInscription terminee\n");
-        } else {
+        printf("\nInscription terminee\n");
+        } 
+        else 
+        {
             printf("\nNiveau invalide. Etudiant non enregistre dans un fichier.\n");
         }
 
@@ -83,7 +135,8 @@ void inscription(struct infopersonnel etudiant[]) {
         printf("Voulez-vous inscrire un autre etudiant ? (o/n) : ");
         scanf(" %c", &reponse);
 
-    } while (reponse == 'o' || reponse == 'O');
+    }
+    while (reponse == 'o' || reponse == 'O');
 }
 
 int main() {
@@ -91,4 +144,3 @@ int main() {
     inscription(etudiant);
     return 0;
 }
-
